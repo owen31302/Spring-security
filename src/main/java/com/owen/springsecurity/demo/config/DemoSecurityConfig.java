4705af8ffd018f4ffd9b08587c13944d5f39b228
yuchlin <yuchlin@ebay.com>
2019-04-01 22:40:20 -0700

    Restrict Access Based on Role.

--- src/main/java/com/owen/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/owen/springsecurity/demo/config/DemoSecurityConfig.java
@@ -12,22 +12,40 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
+
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//		auth.inMemoryAuthentication()
+//				.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//				.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
+//				.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+
 		auth.inMemoryAuthentication().withUser("john").password("secret123").roles("EMPLOYEE");
-		auth.inMemoryAuthentication().withUser("mary").password("secret123").roles("MANAGER");
-		auth.inMemoryAuthentication().withUser("susan").password("secret123").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("mary").password("secret123").roles("EMPLOYEE", "MANAGER");
+		auth.inMemoryAuthentication().withUser("susan").password("secret123").roles("EMPLOYEE", "ADMIN");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-				.anyRequest().authenticated()
+
+				//.anyRequest().authenticated()
+				.antMatchers("/").hasRole("EMPLOYEE")
+				.antMatchers("/leaders/**").hasRole("MANAGER")
+				.antMatchers("/systems/**").hasRole("ADMIN")
+
 				.and()
+
 				.formLogin()
 					.loginPage("/showMyLoginPage")
 					.loginProcessingUrl("/authenticateTheUser")
 					.permitAll()
+
 				.and()
+
 				.logout().permitAll();
 	}
 }
