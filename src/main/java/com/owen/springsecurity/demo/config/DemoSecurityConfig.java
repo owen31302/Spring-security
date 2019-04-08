37b638bb33dcf6cca157acaf4093b1f4332cd2f1
yuchlin <yuchlin@ebay.com>
2019-04-07 22:48:35 -0700

    Spring Security - Add JDBC Database Authentication

--- src/main/java/com/owen/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/owen/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,30 +1,35 @@
 package com.owen.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	// add a reference to our security data source
+
+	@Autowired
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+		// use jdbc authentication ... oh yeah!!!
+
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+
 		// add our users for in memory authentication
 
-//		UserBuilder users = User.withDefaultPasswordEncoder();
-//
-//		auth.inMemoryAuthentication()
-//				.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-//				.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-//				.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
-
-		auth.inMemoryAuthentication().withUser("john").password("secret123").roles("EMPLOYEE");
-		auth.inMemoryAuthentication().withUser("mary").password("secret123").roles("EMPLOYEE", "MANAGER");
-		auth.inMemoryAuthentication().withUser("susan").password("secret123").roles("EMPLOYEE", "ADMIN");
+//		auth.inMemoryAuthentication().withUser("john").password("secret123").roles("EMPLOYEE");
+//		auth.inMemoryAuthentication().withUser("mary").password("secret123").roles("EMPLOYEE", "MANAGER");
+//		auth.inMemoryAuthentication().withUser("susan").password("secret123").roles("EMPLOYEE", "ADMIN");
 	}
 
 	@Override
